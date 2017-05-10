fe6b7b094a75dc723f4f0dd552f2549a6ddf0c3e
Nikki Everett <nikki.everett@outlook.com>
2017-05-10 15:11:53 -0500

    working bolded filter items

--- src/components/SetListComponent.js
+++ src/components/SetListComponent.js
@@ -53,12 +53,14 @@ class SetListComponent extends React.Component {
     if (this.state.sets.list.length === 0) {
       noSetsMessaging = <p>You do not have any sets! Create one.</p>
     }
+    var className = this.state.sets.sortSetsBy === 'name' ? ' sorting by-name' : 'sorting by-count';
+
     return(
       <div className="set-list">
         <h2>Set List</h2>
         {noSetsMessaging}
         <Link to="/create-set" className="create-set">Create new set</Link>
-        <div className="sorting">
+        <div className={className}>
           <div className="by-name" onClick={() => this.sortByName() }>by name</div>
           <div className="by-card-count" onClick={() => this.sortByCardCount() }>by # of cards</div>
         </div>
